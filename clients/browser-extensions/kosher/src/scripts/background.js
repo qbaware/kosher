c691ba694f92741aea671fa0cd4e47f6f2405867
danielgospodinow <danielgospodinow@gmail.com>
2021-11-22 00:39:26 +0200

    Migration extension from V2 to V3

--- clients/browser-extensions/kosher/src/scripts/background.js
+++ clients/browser-extensions/kosher/src/scripts/background.js
@@ -1,15 +1,19 @@
 const tabBackupAction = "tabs_backup"
 
+chrome.runtime.onInstalled.addListener(() => {
+  console.log("Extension starting ...")
+})
+
 chrome.alarms.create(tabBackupAction, {
-    periodInMinutes: 1
+  periodInMinutes: 1
 })
 
 chrome.alarms.onAlarm.addListener((alarm) => {
-    if (alarm.name === tabBackupAction) {
-        chrome.tabs.query({}, (tabs) => {
-            tabs.forEach(tab => {
-                console.log(tab.favIconUrl + " " + tab.title + " " + tab.url)
-            })
-        })
-    }
+  if (alarm.name === tabBackupAction) {
+    chrome.tabs.query({}, (tabs) => {
+      tabs.forEach(tab => {
+        console.log(tab.favIconUrl + " " + tab.title + " " + tab.url)
+      })
+    })
+  }
 })
