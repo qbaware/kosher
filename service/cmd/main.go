d01a87c3a30bf1a46ee1954697aa158224504840
Daniel Gospodinow <danielgospodinow@gmail.com>
2022-11-06 12:11:15 +0200

    Change REST API endpoints

--- service/cmd/main.go
+++ service/cmd/main.go
@@ -21,9 +21,9 @@ func main() {
 	storage := storage.NewInMemoryStorage(maxTabs)
 
 	router.HandleFunc("/tabs", api.GetTabsHandler(storage)).Methods(http.MethodGet)
-	router.HandleFunc("/tab", api.AddTabHandler(storage)).Methods(http.MethodPost)
-	router.HandleFunc("/tab/{id}", api.GetTabHandler(storage)).Methods(http.MethodGet)
-	router.HandleFunc("/tab/{id}", api.RemoveTabHandler(storage)).Methods(http.MethodDelete)
+	router.HandleFunc("/tabs", api.AddTabHandler(storage)).Methods(http.MethodPost)
+	router.HandleFunc("/tabs/{id}", api.GetTabHandler(storage)).Methods(http.MethodGet)
+	router.HandleFunc("/tabs/{id}", api.RemoveTabHandler(storage)).Methods(http.MethodDelete)
 
 	http.Handle("/", router)
 	log.Fatal(http.ListenAndServe(":8080", router))
