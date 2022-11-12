f488d5775e358f8579607c010fdac07bce6813f9
Daniel Gospodinow <danielgospodinow@gmail.com>
2022-11-12 11:08:42 +0200

    Redesign, introduce header

--- clients/browser-extensions/kosher/src/App.js
+++ clients/browser-extensions/kosher/src/App.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-
-import logo from './resources/logo.png';
+import Header from './components/header/Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <div className="Container">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h3>Welcome to Kosher!</h3>
-          </div>
-        </header>
+        <Header></Header>
+        <div className='Content'>
+        </div>
       </div>
     );
   }
---
+++ clients/browser-extensions/kosher/src/components/header/Header.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+import logo from '../../resources/logo.png';
+import NormalText from '../text/NormalText';
+import KosherText from '../text/KosherText';
+
+class Header extends Component {
+  render() {
+    return (
+      <header className="Header">
+        <div className="Container">
+          <img src={logo} className="Logo" alt="logo" />
+          <h3>
+            <NormalText text="kosher"></NormalText>
+          </h3>
+          <h3>
+            <KosherText text="for Chrome"></KosherText>
+          </h3>
+        </div>
+      </header>
+    );
+  }
+}
+
+export default Header;
