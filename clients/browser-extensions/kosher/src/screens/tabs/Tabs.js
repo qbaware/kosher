365d842336d69e059a69b2024764b578725eb888
Daniel Gospodinow <danielgospodinow@gmail.com>
2022-11-13 21:29:50 +0200

    Add a signout button in tabs screen

--- clients/browser-extensions/kosher/src/screens/tabs/Tabs.js
+++ clients/browser-extensions/kosher/src/screens/tabs/Tabs.js
@@ -1,17 +1,42 @@
+/*global chrome*/
 import React from 'react';
 import NamedNavigationalComponent from '../../utils/navigation/NamedNavigationalComponent';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
 import './Tabs.css';
+import App from '../../App';
 
 class Tabs extends NamedNavigationalComponent {
   constructor(props) {
     super(props);
   }
 
+  signOut() {
+    chrome.identity.clearAllCachedAuthTokens(() => {
+      console.log("Purged all cached tokens.")
+      this.setActiveScreen(App.loginScreen);
+    });
+  }
+
   render() {
     return (
-      <div className="Tabs">
-        <h3>Hi tabs!</h3>
-      </div>
+      <Container className="Tabs" component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            paddingTop: "20px",
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center'
+          }}
+        >
+          <Button onClick={this.signOut.bind(this)} variant="contained" color='error'>
+            Sign out
+          </Button>
+        </Box>
+      </Container>
     );
   }
 }
