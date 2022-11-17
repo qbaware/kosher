b2aba9875c140a4d9bd1ebc6a843c03dc64801b7
Daniel Gospodinow <danielgospodinow@gmail.com>
2022-11-17 01:58:12 +0000

    Add commas wherever missing

--- clients/browser-extensions/kosher/src/scripts/background.js
+++ clients/browser-extensions/kosher/src/scripts/background.js
@@ -1,21 +1,21 @@
-const tabBackupAction = "tabs_backup"
+const tabBackupAction = "tabs_backup";
 
 chrome.runtime.onInstalled.addListener(() => {
-  console.log("Extension starting ...")
-})
+  console.log("Extension starting ...");
+});
 
 chrome.alarms.create(tabBackupAction, {
   periodInMinutes: 1
-})
+});
 
 chrome.alarms.onAlarm.addListener((alarm) => {
   if (alarm.name === tabBackupAction) {
     chrome.tabs.query({}, (tabs) => {
-      console.log("Found %d tabs.", tabs.length)
+      console.log("Found %d tabs.", tabs.length);
 
       tabs.forEach(tab => {
-        const tabInfo = tab.favIconUrl + " " + tab.title + " " + tab.url
-      })
-    })
+        const tabInfo = tab.favIconUrl + " " + tab.title + " " + tab.url;
+      });
+    });
   }
-})
+});
